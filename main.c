a8e781cf17c98262cc4eee2e9eaa069e646439f7
Dmitry96s <dmitrij.96s@gmail.com>
2017-12-27 02:03:39 +0300

    Changed password and buffer size

--- main.c
+++ main.c
@@ -12,13 +12,13 @@ int main(void)
 int buff_overflow_test()
 {
 
-    char buff_var4[4];
+    char buff_var4[8];
     int pass = 0;
 
     printf("\n Enter the password : \n");
     gets(buff_var4);
 
-    if(strcmp(buff_var4, "pasw"))
+    if(strcmp(buff_var4, "password"))
     {
         printf ("\n Wrong Password \n");
     }
